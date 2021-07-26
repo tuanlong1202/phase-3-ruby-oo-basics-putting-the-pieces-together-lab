678f5a18575296658c702dcc1435a3daf16d4242
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-26 17:11:13 -0400

    Phase 3 ruby oo basics putting the pirces together lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand_name)
+        @brand = brand_name
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
